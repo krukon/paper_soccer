14d70c2b945c5aa58167cf067e14e2bff2e76678
Wojtek Kruk <krukon@gmail.com>
2014-05-04 22:32:44 +0200

    Model: added movement with validation

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -12,20 +12,68 @@ public class Board {
 	 * Enum representing directions of movements on the field.
 	 */
 	public static enum Direction {
-		N, NE, E, SE, S, SW, W, NW
+		N, NE, E, SE, S, SW, W, NW;
+
+		private static int[] deltaX = {0, 1, 1, 1, 0, -1, -1, -1};
+		private static int[] deltaY = {1, 1, 0, -1, -1, -1, 0, 1};
+
+		/**
+		 * Get direction of a move from (x1, y1) to (x2, y2)
+		 *
+		 * @param x1 x coordinate of start point
+		 * @param y1 y coordinate of start point
+		 * @param x2 x coordinate of end point
+		 * @param y2 y coordinate of end point
+		 * @return Direction of movement
+		 */
+		public static Direction getDirection(int x1, int y1, int x2, int y2) {
+			for (int i = 0; i < 8; i++)
+				if (x2 - x1 == deltaX[i] && y2 - y1 == deltaY[i])
+					return Direction.values()[i];
+			return null;
+		}
+
+		/**
+		 * Get opposite direction
+		 *
+		 * @return opposite direction
+		 */
+		public Direction opposite() {
+			return Direction.values()[(4 + this.ordinal()) % 8];
+		}
 	}
 	
 	private int width;
 	private int height;
 	private int field[][];
+	private int headX;
+	private int headY;
+	private boolean gameOver;
 	
 	public Board(int width, int height) {
 		this.width = validSize(width) / 2;
 		this.height = validSize(height) / 2;
-		field = new int[2 * this.width + 1][2 * this.height + 1];
+		field = new int[2 * this.width + 1][2 * this.height + 3];
 		boundField();
+		headX = 0;
+		headY = 0;
+		gameOver = false;
 	}
 	
+	/**
+	 * Check if move to (x, y) is valid
+	 *
+	 * @param x x coordinate of end point
+	 * @param y y coordinate of end point
+	 * @return true if the move can be made
+	 */
+	public boolean canMoveTo(int x, int y) {
+		Direction dir = Direction.getDirection(headX, headY, x, y);
+		if (gameOver || dir == null || !isInBounds(x, y))
+			return false;
+		return (field[getX(headX)][getY(headY)] & (1 << dir.ordinal())) == 0;
+	}
+
 	/**
 	 * @return full height of the field
 	 */
@@ -38,6 +86,39 @@ public class Board {
 	 */
 	public int getWidth() {
 		return width * 2;
+	}
+
+	/**
+	 * Check if the game is over
+	 *
+	 * @return true if the game is over
+	 */
+	public boolean isGameOver() {
+		return gameOver;
+	}
+
+	/**
+	 * Mark movement to point (x, y)
+	 *
+	 * @param x x coordinate of end point
+	 * @param y y coordinate of end point
+	 * @return true if this move will grant additional move to current player
+	 * @throws IllegalMove
+	 */
+	public boolean moveTo(int x, int y) throws IllegalMove {
+		if (!canMoveTo(x, y))
+			throw new IllegalMove();
+		boolean bounce = wasVisited(x, y);
+		Direction dir = Direction.getDirection(headX, headY, x, y);
+		setConnection(headX, headY, dir);
+		setConnection(x, y, dir.opposite());
+		headX = x;
+		headY = y;
+		if ((Math.abs(headX) <= 1 && Math.abs(headY) == height + 1) || field[getX(headX)][getY(headY)] == (1 << 8) - 1) {
+			gameOver = true;
+			return false;
+		}
+		return bounce;
 	}
 
 	/**
@@ -94,7 +175,7 @@ public class Board {
 	 * @return correct field index for y
 	 */
 	private int getY(int y) {
-		return height + y;
+		return height + y + 1;
 	}
 
 	/**
@@ -104,7 +185,20 @@ public class Board {
 	 * @return true if the point is in the field bounds
 	 */
 	private boolean isInBounds(int x, int y) {
-		return Math.abs(x) <= width && Math.abs(y) <= height;
+		return (Math.abs(x) <= width && Math.abs(y) <= height) || (Math.abs(x) <= 1 && Math.abs(y) == height + 1);
+	}
+
+	/**
+	 * Check if the point (x, y) was visited
+	 *
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 * @return true if the point was visited
+	 */
+	private boolean wasVisited(int x, int y) {
+		if (!isInBounds(x, y))
+			return false;
+		return field[getX(x)][getY(y)] > 0;
 	}
 
 	/**
