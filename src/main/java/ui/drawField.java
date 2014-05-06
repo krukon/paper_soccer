50eb7b8d14f903d5fb96e47d5d88aeba81637235
Mateusz Cianciara <e.cianciara@gmail.com>
2014-05-06 13:23:17 +0200

    added resize

--- src/main/java/ui/drawField.java
+++ src/main/java/ui/drawField.java
@@ -1,8 +1,8 @@
 package ui;
 import java.lang.*;
-import java.util.ListIterator;
-
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -12,46 +12,81 @@ import javafx.stage.Stage;
 /**
  * Created by mateusz on 06.05.2014.
  */
-public class drawField extends Application{
+public class drawField extends Application {
     public static String windowName;
     public static int height, width;
     private Stage stage;
-    private double xZero, yZero;
+    private Scene scene;
+    //private double xZero, yZero;
     private double pixelHeight = 600, pixelWidth = 400;
     private double crateSize;
     private Group root;
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         drawField.width = 8;
         drawField.height = 10;
         launch(args);
     }
+
     @Override
     public void start(final Stage primaryStage) {
         stage = primaryStage;
         root = new Group();
         stage.setTitle(windowName);
-        stage.setScene(new Scene(root, pixelWidth, pixelHeight));
-        xZero = width/2;
-        yZero = height/2;
-        crateSize = Math.min(pixelHeight/((double)(drawField.height+4)), pixelWidth/((double)(drawField.width+2)));
+        scene = new Scene(root, pixelWidth, pixelHeight, Color.GREEN);
+        stage.setScene(scene);
+        //xZero = width/2;
+        //yZero = height/2;
+        crateSize = Math.min(pixelHeight/((double)(drawField.height+4)), pixelWidth/((double)(drawField.width+4)));
         System.out.println(crateSize);
         stage.show();
+
         this.field();
+        scene.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+                System.out.println("Width: " + newSceneWidth);
+                pixelWidth = newSceneWidth.doubleValue();
+                crateSize = Math.min(pixelHeight/((double)(drawField.height+4)), pixelWidth/((double)(drawField.width+4)));
+                root.getChildren().removeAll();
+                root.getChildren().clear();
+                field();
+            }
+        });
+        scene.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+                System.out.println("Height: " + newSceneHeight);
+                pixelHeight = newSceneHeight.doubleValue();
+                crateSize = Math.min(pixelHeight/((double)(drawField.height+4)), pixelWidth/((double)(drawField.width+4)));
+                root.getChildren().removeAll();
+                root.getChildren().clear();
+                field();
+            }
+        });
     }
-    private double translateX(int x){ //return pixel x dim
+
+    private double translateX(int x) { //return pixel x dim
         return (pixelWidth/2)+crateSize*x;
     }
-    private double translateY(int y){ //return pixel y dim
+
+    /**
+     *
+     * @param y
+     * @return pixel y dim
+     */
+    private double translateY(int y) {
         return (pixelHeight/2)-crateSize*y;
     }
-    private void drawLine(int x1, int y1, int x2, int y2){
+
+    private void drawLine(int x1, int y1, int x2, int y2) {
         Line line = new Line(translateX(x1), translateY(y1), translateX(x2), translateY(y2));
-        line.setStroke(Color.BLUE);
+        line.setStroke(Color.WHITE);
         line.setStrokeWidth(2.5);
-        root.getChildren().add(line);//new Line(translateX(x1), translateY(y1), translateX(x2), translateY(y2)));
+        root.getChildren().add(line);
         System.out.println(translateX(x1)+" " + translateY(y1)+" "+ translateX(x2)+ " "+ translateY(y2));
     }
-    private void field(){
+
+    // drawField
+    private void field() {
         int x = width / 2;
         int y = height / 2;
         drawLine(-x, y, -1, y);
@@ -69,16 +104,18 @@ public class drawField extends Application{
         drawPoints();
     }
 
-    private void drawPoints(){
-        for(int i = 0; i<=width; i++){
-            for(int j = 0; j<=height; j++){
+    private void drawPoints() {
+        for(int i = -1; i<=width+1; i++){
+            for(int j = -2; j<=height+2; j++){
                 Point(-width/2+i, -height/2+j);
             }
         }
     }
-    private void Point(int x, int y){
+
+    // drawPoint
+    private void Point(int x, int y) {
         Line line = new Line(translateX(x), translateY(y), translateX(x), translateY(y));
-        line.setStroke(Color.BLUE);
+        line.setStroke(Color.WHITE);
         if(x==0 && y==0) line.setStrokeWidth(4.0);
         else line.setStrokeWidth(2.0);
         root.getChildren().add(line);
