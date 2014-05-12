956b6d27917d573cce9b7dfd9a73008d874d0c56
Jakub <jakub.nowak94@interia.pl>
2014-05-12 23:16:25 +0200

    Added workflow

---
+++ src/main/java/ui/GameResultDialog.java
@@ -0,0 +1,125 @@
+package ui;
+
+/**
+ * Class representing a simple dialog box with game result.
+ * 
+ * @author jakub
+ */
+
+import controller.PaperSoccer;
+
+import helpers.GameResult;
+import helpers.Move;
+import helpers.Player;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class GameResultDialog extends Stage {
+	
+	public GameResultDialog(GameResult result) {
+		initModality(Modality.APPLICATION_MODAL);
+		
+		Label message = new Label("Won " + result.getWinner().getName());
+		message.setFont(Font.font(30));
+		message.setAlignment(Pos.BASELINE_CENTER);
+		
+		Button rematch = new Button("Rematch");
+		rematch.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Rematch");
+				// TODO start new game				
+			}
+		});
+		
+		Button exit = new Button("Main menu");
+		exit.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Back to main menu");
+				PaperSoccer.getMainWindow().showMenu();
+				close();
+			}
+		});
+		
+		HBox hBox = new HBox();
+        hBox.setAlignment(Pos.BASELINE_CENTER);
+        hBox.setSpacing(40.0);
+        hBox.getChildren().addAll(rematch, exit);
+
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.BASELINE_CENTER);
+        vBox.setSpacing(40.0);
+        vBox.getChildren().addAll(message, hBox);
+
+        setScene(new Scene(vBox, 300, 150));
+	}
+	/**
+	 * @throws InterruptedException 
+	 * @deprecated
+	 * Only for game result dialog tests.
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				MainWindow.startNewGame(args);
+			}	
+		}).start();
+		Thread.sleep(2000);
+		Platform.runLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				Player p = new Player() {
+
+					@Override
+					public void startNewGame(int width, int height) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void finishGame(GameResult result) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public Move getNextMove() {
+						// TODO Auto-generated method stub
+						return null;
+					}
+
+					@Override
+					public void registerMove(Move move) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public String getName() {
+						// TODO Auto-generated method stub
+						return "XXX";
+					}
+					
+				};
+				new GameResultDialog(new GameResult(p, 1, 1)).show();
+				
+			}
+		});
+	}
+}
--- src/main/java/ui/TwoPlayersWindow.java
+++ src/main/java/ui/TwoPlayersWindow.java
@@ -2,51 +2,51 @@ package ui;
 
 /**
  * Class representing a multiplayer offline window.
+ * 
  * @author jakub
  */
 
-import javafx.application.Application;
+import controller.PaperSoccer;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
-public class TwoPlayers extends Application {
-	private String appTitle = "Paper soccer";
+public class TwoPlayersWindow extends BorderPane {
 	private Text windowTitle = new Text("Enter your names \n and board size");
 	private Insets insets = new Insets(25, 25, 25, 25);
 	
-	private TextField playerOneName = new TextField();
-	private TextField playerTwoName = new TextField();
-	private TextField boardWidth = new TextField();
-	private TextField boardHeight = new TextField();
-
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle(appTitle);
+	private TextField playerOneName;
+	private TextField playerTwoName;
+	private TextField boardWidth;
+	private TextField boardHeight;
+	
+	public TwoPlayersWindow() {
+		playerOneName = new TextField();
+		playerTwoName = new TextField();
+		boardWidth = new TextField();
+		boardHeight = new TextField();
 		
-		BorderPane border = new BorderPane();
-		border.setPadding(insets);
+		windowTitle.setFill(Color.WHITE);
 		
-		border.setTop(addWindowTitle());
-		border.setCenter(addGridPane());
-		border.setBottom(addStartButton());
-		
-		Scene scene = new Scene(border);
-		primaryStage.setScene(scene);
-		
-		primaryStage.show();
+		setPadding(insets);
+		setTop(addWindowTitle());
+		setCenter(addGridPane());
+		setBottom(addStartBackButton());
 	}
 	
+	/**
+	 * Constructs window title.
+	 * @return HBox with a title
+	 */
 	private HBox addWindowTitle() {
 		HBox textBox = new HBox();
 		textBox.setAlignment(Pos.CENTER);
@@ -55,41 +55,102 @@ public class TwoPlayers extends Application {
 		return textBox;
 	}
 	
-	private HBox addStartButton() {
+	/**
+	 * Constructs a start button.
+	 * @return constructed button
+	 */
+	private Button getStartButton() {
 		Button startButton = new Button("START");
 		startButton.setMinSize(100, 50);
 		
 		startButton.setOnAction(new EventHandler<ActionEvent>() {
 			
-			//TODO Check names and board size
+			//TODO Showing board and size validation
 			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Button pressed");	
+			public void handle(ActionEvent event) {				
+				try {
+					int width = Integer.parseInt(boardWidth.getCharacters().toString());
+					int height = Integer.parseInt(boardHeight.getCharacters().toString());
+					
+					System.out.println("Constructing board " + width + " x " + height);					
+				} catch (Exception e) {
+					System.out.println("Size is not an integer");
+				}
+			
 			}
 		});
 		
+		return startButton;
+	}
+	
+	/**
+	 * Constructs an exit button.
+	 * @return constructed button
+	 */
+	private Button getBackButton() {
+		Button exitButton = new Button("BACK");
+		exitButton.setMinSize(100, 50);
+		
+		exitButton.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {				
+				System.out.println("Back to main menu");
+				PaperSoccer.getMainWindow().showMenu();
+			
+			}
+		});
+		
+		return exitButton;
+	}
+	
+	/**
+	 * Adds start and exit buttons to the window.
+	 * @return HBox with a buttons
+	 */
+	private HBox addStartBackButton() {
 		HBox buttonBox = new HBox();
 		buttonBox.setAlignment(Pos.CENTER);
-		buttonBox.getChildren().add(startButton);
+		buttonBox.getChildren().addAll(getStartButton(), getBackButton());
 		
 		return buttonBox;
 	}
 	
+	/**
+	 * Adds labels for text fields to grid.
+	 * @param grid grid to place labels
+	 */
 	private void addPlayersLabels(GridPane grid) {
 		Label playerOne = new Label("Player one:");
 		Label playerTwo = new Label("Player two:");
+		
+		playerOne.setTextFill(Color.WHITE);
+		playerTwo.setTextFill(Color.WHITE);
+		
 		grid.add(playerOne, 0, 0);
 		grid.add(playerTwo, 0, 1);
 	}
 	
+	/**
+	 * Adds text fields to grid. 
+	 * @param grid grid to place text fields
+	 */
 	private void addPlayersTextFields(GridPane grid) {
 		grid.add(playerOneName, 1, 0);
 		grid.add(playerTwoName, 1, 1);
 	}
 	
+	/**
+	 * Adds board text fields and labels to grid.
+	 * @param grid grid to place text fields and labels
+	 */
 	private void addBoardSize(GridPane grid) {
 		Label width = new Label("Width:");
 		Label height = new Label("Height");
+		
+		width.setTextFill(Color.WHITE);
+		height.setTextFill(Color.WHITE);
+		
 		grid.add(width, 0, 5);
 		grid.add(height, 0, 6);
 		
@@ -97,6 +158,10 @@ public class TwoPlayers extends Application {
 		grid.add(boardHeight, 1, 6);
 	}
 	
+	/**
+	 * Constructs base grid and its components for window.
+	 * @return constructed grid with components
+	 */
 	private GridPane addGridPane() {
 		GridPane grid = new GridPane();
 		grid.setMinSize(300, 400);
