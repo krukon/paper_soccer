286502f21bb4ee7f09c1f2bee6b4104e1e54daa2
Wiktor Tendera <wiktortendera@gmail.com>
2014-05-19 14:52:03 +0200

    Moved computing strategies to StrategyBot class

--- src/main/java/bots/DefensiveBot.java
+++ src/main/java/bots/DefensiveBot.java
@@ -1,19 +1,12 @@
 package bots;
 
-import java.util.Stack;
 import java.util.concurrent.Callable;
 
-import model.Board.Direction;
-import model.IllegalMove;
-
-import helpers.Point;
 
 public final class DefensiveBot extends StrategyBot {
 	public static final String name ="Defensive bot";
 
-	private Stack<Point> currentMoves = new Stack<>();
-	private Stack<Point> bestMoves = new Stack<>();
-	private boolean isWinningMove;
+
 	
 	@Override
 	public String getName() {
@@ -48,119 +41,4 @@ public final class DefensiveBot extends StrategyBot {
 		});
 	}
 	
-	private void computeDefensiveStrategy() {
-		//System.out.println("Defensive");
-		currentMoves.clear();
-		bestMoves.clear();
-		currentMoves.push(board.getCurrent());
-		isWinningMove = false;
-		backTrackDefensive();
-		//if (isWinningMove)
-		//	System.out.println("Winning (" + bestMoves.size() + ") :" + bestMoves);
-		for(int i = 0; i < bestMoves.size(); i++)
-			nextMoves.add(bestMoves.get(i));
-	}
-	
-	private void backTrackDefensive() {
-		Point current = board.getCurrent();
-		for(Direction dir : Direction.permuteValues()) {
-			Point target = dir.moveFrom(current);
-			if(board.canMoveTo(target.x, target.y) && isAcceptableMove(target)) {
-				try {
-					boolean bounce = board.moveTo(target.x, target.y);
-					currentMoves.push(target);
-					if (bounce)
-						backTrackDefensive();
-					else
-						copyStacksDefensive();
-					try {
-						board.moveBack();
-					} catch (IllegalMove e) {}
-					currentMoves.pop();
-				} catch (IllegalMove e) { System.err.println("Moving to gone wrong!" + e.getMessage());  }
-			}
-		}
-	}
-	
-	private void copyStacksDefensive() {
-		if (isWinningMove)
-			return;
-		if(currentMoves.size() > bestMoves.size() || (currentMoves.size() == bestMoves.size() && rg.nextDouble() < 0.3)) {
-			bestMoves.clear();
-			for(int i = 1; i < currentMoves.size(); i++)
-				bestMoves.push(currentMoves.get(i));
-		}
-		if (currentMoves.lastElement().y == getOpponentGoalY())
-			isWinningMove = true;
-	}
-	
-	private void copyStacksOffensive() {
-		if (isWinningMove)
-			return;
-		int d1 = getDistance(currentMoves.lastElement()), d2;
-		if(bestMoves.size() == 0 || d1 < (d2 = getDistance(bestMoves.lastElement())) || (d1 == d2 && rg.nextDouble() < 0.3)) {
-			bestMoves.clear();
-			for(int i = 1; i < currentMoves.size(); i++)
-				bestMoves.push(currentMoves.get(i));
-		}
-		if (currentMoves.lastElement().y == getOpponentGoalY())
-			isWinningMove = true;
-	}
-	
-	private void computeOffensiveStrategy() {
-		//System.out.println("Offensive");
-		currentMoves.clear();
-		bestMoves.clear();
-		currentMoves.push(board.getCurrent());
-		isWinningMove = false;
-		backTrackOffensive();
-		//if (isWinningMove)
-		//	System.out.println("Winning (" + bestMoves.size() + ") :" + bestMoves);
-		for(int i = 0; i < bestMoves.size(); i++)
-			nextMoves.add(bestMoves.get(i));
-	}
-	
-	private int getDistance(Point target) {
-		return (target.x)*(target.x) + (target.y - getOpponentGoalY())*(target.y - getOpponentGoalY());
-	}
-
-	private void backTrackOffensive() {
-		Point current = board.getCurrent();
-		for(Direction dir : Direction.permuteValues()) {
-			Point target = dir.moveFrom(current);
-			if(board.canMoveTo(target.x, target.y) && isAcceptableMove(target)) {
-				try {
-					boolean bounce = board.moveTo(target.x, target.y);
-					currentMoves.push(target);
-					if (bounce)
-						backTrackOffensive();
-					else
-						copyStacksOffensive();
-					try {
-						board.moveBack();
-					} catch (IllegalMove e) {}
-					currentMoves.pop();
-				} catch (IllegalMove e) { System.err.println("Moving to gone wrong!" + e.getMessage());  }
-			}
-		}
-	}
-	
-	
-	private boolean isAcceptableMove(Point target) {
-		boolean canMove = board.canMoveTo(target.x, target.y),
-				ownGoal = target.y == getMyGoalY(),
-				isLocking = board.isLockingMove(board.getCurrent(), target);
-		if (!canMove || ownGoal || isLocking)
-			return false;
-		return true;
-	}
-	
-	private void computeRandomStrategy() {
-		//System.out.println("Random");
-		Point target;
-		do {
-			target = Direction.values()[rg.nextInt(8)].moveFrom(board.getCurrent());
-		} while (!isAcceptableMove(target) && board.availableMovesCount(board.getCurrent()) > 1);
-		nextMoves.add(target);
-	}
 }
--- src/main/java/bots/StrategyBot.java
+++ src/main/java/bots/StrategyBot.java
@@ -5,10 +5,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Random;
+import java.util.Stack;
 import java.util.concurrent.Callable;
 
 import model.BotBoard;
 import model.IllegalMove;
+import model.Board.Direction;
 import helpers.GameResult;
 import helpers.Move;
 import helpers.Player;
@@ -17,6 +19,9 @@ import helpers.Point;
 public abstract class StrategyBot implements Player {
 	private List<Double> strategyProbabilities = new ArrayList<>();
 	private List<Callable<?>> strategies = new ArrayList<>();
+	private Stack<Point> currentMoves = new Stack<>();
+	private Stack<Point> bestMoves = new Stack<>();
+	private boolean isWinningMove;
 	
 	protected int width;
 	protected int height;
@@ -62,6 +67,41 @@ public abstract class StrategyBot implements Player {
 		} catch (IllegalMove e) { }
 	}
 	
+	void computeOffensiveStrategy() {
+		//System.out.println("Offensive");
+		currentMoves.clear();
+		bestMoves.clear();
+		currentMoves.push(board.getCurrent());
+		isWinningMove = false;
+		backTrackOffensive();
+		//if (isWinningMove)
+		//	System.out.println("Winning (" + bestMoves.size() + ") :" + bestMoves);
+		for(int i = 0; i < bestMoves.size(); i++)
+			nextMoves.add(bestMoves.get(i));
+	}
+	
+	void computeDefensiveStrategy() {
+		//System.out.println("Defensive");
+		currentMoves.clear();
+		bestMoves.clear();
+		currentMoves.push(board.getCurrent());
+		isWinningMove = false;
+		backTrackDefensive();
+		//if (isWinningMove)
+		//	System.out.println("Winning (" + bestMoves.size() + ") :" + bestMoves);
+		for(int i = 0; i < bestMoves.size(); i++)
+			nextMoves.add(bestMoves.get(i));
+	}
+	
+	void computeRandomStrategy() {
+		//System.out.println("Random");
+		Point target;
+		do {
+			target = Direction.values()[rg.nextInt(8)].moveFrom(board.getCurrent());
+		} while (!isAcceptableMove(target) && board.availableMovesCount(board.getCurrent()) > 1);
+		nextMoves.add(target);
+	}
+	
 	protected int getMyGoalY() {
 		return (topGoal ? 1 : -1) * (board.getHeight() / 2 +1);
 	}
@@ -85,5 +125,90 @@ public abstract class StrategyBot implements Player {
 				} catch (Exception e) {}
 		}
 	}
+	
+	
+	private void backTrackDefensive() {
+		Point current = board.getCurrent();
+		for(Direction dir : Direction.permuteValues()) {
+			Point target = dir.moveFrom(current);
+			if(board.canMoveTo(target.x, target.y) && isAcceptableMove(target)) {
+				try {
+					boolean bounce = board.moveTo(target.x, target.y);
+					currentMoves.push(target);
+					if (bounce)
+						backTrackDefensive();
+					else
+						copyStacksDefensive();
+					try {
+						board.moveBack();
+					} catch (IllegalMove e) {}
+					currentMoves.pop();
+				} catch (IllegalMove e) { System.err.println("Moving to gone wrong!" + e.getMessage());  }
+			}
+		}
+	}
+	
+	private void copyStacksDefensive() {
+		if (isWinningMove)
+			return;
+		if(currentMoves.size() > bestMoves.size() || (currentMoves.size() == bestMoves.size() && rg.nextDouble() < 0.3)) {
+			bestMoves.clear();
+			for(int i = 1; i < currentMoves.size(); i++)
+				bestMoves.push(currentMoves.get(i));
+		}
+		if (currentMoves.lastElement().y == getOpponentGoalY())
+			isWinningMove = true;
+	}
+	
+	private void copyStacksOffensive() {
+		if (isWinningMove)
+			return;
+		int d1 = getDistance(currentMoves.lastElement()), d2;
+		if(bestMoves.size() == 0 || d1 < (d2 = getDistance(bestMoves.lastElement())) || (d1 == d2 && rg.nextDouble() < 0.3)) {
+			bestMoves.clear();
+			for(int i = 1; i < currentMoves.size(); i++)
+				bestMoves.push(currentMoves.get(i));
+		}
+		if (currentMoves.lastElement().y == getOpponentGoalY())
+			isWinningMove = true;
+	}
+	
+	
+	
+	private int getDistance(Point target) {
+		return (target.x)*(target.x) + (target.y - getOpponentGoalY())*(target.y - getOpponentGoalY());
+	}
+
+	private void backTrackOffensive() {
+		Point current = board.getCurrent();
+		for(Direction dir : Direction.permuteValues()) {
+			Point target = dir.moveFrom(current);
+			if(board.canMoveTo(target.x, target.y) && isAcceptableMove(target)) {
+				try {
+					boolean bounce = board.moveTo(target.x, target.y);
+					currentMoves.push(target);
+					if (bounce)
+						backTrackOffensive();
+					else
+						copyStacksOffensive();
+					try {
+						board.moveBack();
+					} catch (IllegalMove e) {}
+					currentMoves.pop();
+				} catch (IllegalMove e) { System.err.println("Moving to gone wrong!" + e.getMessage());  }
+			}
+		}
+	}
+	
+	
+	private boolean isAcceptableMove(Point target) {
+		boolean canMove = board.canMoveTo(target.x, target.y),
+				ownGoal = target.y == getMyGoalY(),
+				isLocking = board.isLockingMove(board.getCurrent(), target);
+		if (!canMove || ownGoal || isLocking)
+			return false;
+		return true;
+	}
+	
 	
 }
